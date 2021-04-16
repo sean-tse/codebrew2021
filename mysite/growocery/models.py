d4844647dcd84626fa303d95d9ebc01c8f44d050
sean tse <seantse3486784401@gmail.com>
2021-04-16 21:19:40 +1000

    created model for customer profiles

--- mysite/growocery/models.py
+++ mysite/growocery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+class CustomerProfile(models.Model):
+    customerAccount = models.OneToOneField(User, on_delete=models.CASCADE)
+    addressLine = models.CharField(max_length=100, blank=True)
+    postcode = models.CharField(max_length=4, validators=[RegexValidator(r'^\d{1,10}$')], blank=True)
+    phone = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')], blank=True)
+    bsb = models.CharField(max_length=6, validators=[RegexValidator(r'^\d{1,10}$')], blank=True)
+    bankAccount = models.CharField(max_length=15, validators=[RegexValidator(r'^\d{1,10}$')], blank=True)
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        CustomerProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.customerprofile.save()    
