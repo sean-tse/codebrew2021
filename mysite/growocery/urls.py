b545da2e7ef9ea529087f39a1c24622ed6d2c953
APKA4A7YDNJDHHNN4FHR <zyng@student.unimelb.edu.au>
2021-04-17 01:46:34 +1000

    styled register page

--- mysite/growocery/urls.py
+++ mysite/growocery/urls.py
@@ -2,9 +2,17 @@ from django.urls import path
 
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
     path('register/', views.register, name='register')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/\
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-STATIC_URL = f'{BASE_DIR}/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
